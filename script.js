74f613520aa87395538141d85be3f363c977f4b6
oreotamish <sinhatamish@gmail.com>
2024-11-06 00:54:31 +0530

    chore: added checks for socket hangup handling

--- script.js
+++ script.js
@@ -4,13 +4,19 @@ const socket = io("http://localhost:8080");
 
 socket.on("connect", () => {
   console.log("Connected to server");
-  setTimeout(() => {
-    socket.emit("processUrl", { url: "wss://socket3.lichess.org/play/NBQc5D25qmv3/v6?sri=hmj1bCoB5cih&v=0" });
-  }, 2000);
+  setInterval(() => {
+    socket.emit("processUrl", { url: "wss://socket0.lichess.org/play/lsHZ978wFqef/v6?sri=2CgEiBIyV9Be&v=0" });
+  }, 5000);//   setInterval(() => {
+//     socket.emit("processUrl", { url: "wss://socket0.lichess.org/play/G4tNb0ndac4x/v6?sri=NIv0_NUeN1e8&v=0" });
+//   }, 5000);
+
 });
 
+let processedData;
 socket.on("processedData", (data) => {
-  console.log("best move:", data);
+    if (JSON.stringify(data) == JSON.stringify(processedData)) {}
+    else console.log("best move:", data);
+    processedData = data;
 });
 
 socket.on("error", (error) => {
